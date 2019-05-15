018aef1484cc0e335d5974f2478a88f1b042b853
MaanuelMM <manuel.martin.malagon@alumnos.upm.es>
2019-05-15 13:47:18 +0200

    Add AtomicScript and CollisionController

---
+++ Assets/Resources/AtomicScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class AtomicScript : MonoBehaviour, ITrackableEventHandler {
+
+	private bool present = false;
+
+	private TrackableBehaviour mTrackableBehaviour;
+
+	void Start ()
+	{
+		mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+		if (mTrackableBehaviour)
+		{
+			mTrackableBehaviour.RegisterTrackableEventHandler(this);
+		}
+	}
+
+	public void OnTrackableStateChanged (TrackableBehaviour.Status previousStatus,
+		TrackableBehaviour.Status newStatus)
+	{
+		if (newStatus == TrackableBehaviour.Status.DETECTED ||
+			newStatus == TrackableBehaviour.Status.TRACKED ||
+			newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+		{
+			present = true;
+		}
+		else
+		{
+			present = false;
+		}
+	}
+
+	public bool isPresent ()
+	{
+		return present;
+	}
+
+}
---
+++ Assets/Resources/CollisionController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionController : MonoBehaviour {
+
+	public GameObject H, C, N, O, Na, Cl;
+
+	// Use this for initialization.
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Debug.Log (Vector3.Distance(H.transform.position, O.transform.position));
+		Debug.Log (H.GetComponent<AtomicScript> ().isPresent ());
+		Debug.Log (O.GetComponent<AtomicScript> ().isPresent ());
+		if (Vector3.Distance (H.transform.position, O.transform.position) < 10f & H.GetComponent<AtomicScript> ().isPresent () & O.GetComponent<AtomicScript> ().isPresent ()) {
+			H.transform.GetChild (0).gameObject.SetActive (false);
+			O.transform.GetChild (0).gameObject.SetActive (false);
+		} else {
+			H.transform.GetChild (0).gameObject.SetActive (true);
+			O.transform.GetChild (0).gameObject.SetActive (true);
+		}
+	}
+}
