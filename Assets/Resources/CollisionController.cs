ee24e2be08eb99ec06762eaad26e067a84efb19e
MaanuelMM <manuel.martin.malagon@alumnos.upm.es>
2019-05-15 21:25:52 +0200

    Add H2O molecule

--- Assets/Resources/CollisionController.cs
+++ Assets/Resources/CollisionController.cs
@@ -6,6 +6,10 @@ public class CollisionController : MonoBehaviour {
 
 	public GameObject H, C, N, O, Na, Cl;
 
+	public GameObject H2OPrefab, CH4Prefab, NH3Prefab, COPrefab, CO2Prefab, NaClPrefab;
+
+	private GameObject H2O, CH4, NH3, CO, CO2, NaCl;
+
 	// Use this for initialization.
 	void Start () {
 		
@@ -13,13 +17,36 @@ public class CollisionController : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		Debug.Log (Vector3.Distance(H.transform.position, O.transform.position));
-		Debug.Log (H.GetComponent<AtomicScript> ().isPresent ());
-		Debug.Log (O.GetComponent<AtomicScript> ().isPresent ());
-		if (Vector3.Distance (H.transform.position, O.transform.position) < 10f & H.GetComponent<AtomicScript> ().isPresent () & O.GetComponent<AtomicScript> ().isPresent ()) {
-			H.transform.GetChild (0).gameObject.SetActive (false);
-			O.transform.GetChild (0).gameObject.SetActive (false);
-		} else {
+		if (Vector3.Distance (H.transform.position, O.transform.position) < 10f
+		    & H.GetComponent<AtomicScript> ().isPresent ()
+		    & O.GetComponent<AtomicScript> ().isPresent ())
+		{
+			if (!GameObject.Find ("H2O")) {
+				H.transform.GetChild (0).gameObject.SetActive (false);
+				O.transform.GetChild (0).gameObject.SetActive (false);
+				H2O = (GameObject)Instantiate (H2OPrefab, new Vector3 ((H.transform.position.x + O.transform.position.x) / 2f,
+					(H.transform.position.y + O.transform.position.y) / 2f,
+					(H.transform.position.z + O.transform.position.z) / 2f),
+					new Quaternion ((H.transform.rotation.x + O.transform.rotation.x) / 2f,
+						(H.transform.rotation.y + O.transform.rotation.y) / 2f,
+						(H.transform.rotation.z + O.transform.rotation.z) / 2f,
+						(H.transform.rotation.w + O.transform.rotation.w) / 2f));
+				H2O.name = "H2O";
+			}
+			else
+			{
+				H2O.transform.position = new Vector3 ((H.transform.position.x + O.transform.position.x) / 2f,
+					(H.transform.position.y + O.transform.position.y) / 2f,
+					(H.transform.position.z + O.transform.position.z) / 2f);
+				H2O.transform.rotation = new Quaternion ((H.transform.rotation.x + O.transform.rotation.x) / 2f,
+					(H.transform.rotation.y + O.transform.rotation.y) / 2f,
+					(H.transform.rotation.z + O.transform.rotation.z) / 2f,
+					(H.transform.rotation.w + O.transform.rotation.w) / 2f);
+			}
+		}
+		else if (GameObject.Find ("H2O"))
+		{
+			Destroy (H2O);
 			H.transform.GetChild (0).gameObject.SetActive (true);
 			O.transform.GetChild (0).gameObject.SetActive (true);
 		}
